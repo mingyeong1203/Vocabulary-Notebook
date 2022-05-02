254c511dc3ff8047b0e2f635668dc11873e30a19
DESKTOP-4EJAK4E\mingy <mingyeonging1203@gmail.com>
2022-05-02 19:53:02 +0900

    🔒️개인 정보 숨김

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import requests
 
 app = Flask(__name__)
 
-client = MongoClient('mongodb://test:test@15.164.98.36', 27017)
+client = MongoClient('아이피', 27017, username="test", password="test")
 db = client.dbsparta_plus_week2
 
 
@@ -22,7 +22,7 @@ def detail(keyword):
     # API에서 단어 뜻 찾아서 결과 보내기
     status_receive = request.args.get("status_give", "old")
     r = requests.get(f"https://owlbot.info/api/v4/dictionary/{keyword}",
-                     headers={"Authorization": "Token fa94f4b391cbaf92d16b54ca28dc8ea4b09dc478"})
+                     headers={"Authorization": "Token 토큰"})
     if r.status_code != 200:
         return redirect(url_for("main", msg="Word not found in dictionary; Try another word"))
     result = r.json()
