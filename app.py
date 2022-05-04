1b22d05bcafaae98447aec16cb56173c9293a9c8
DESKTOP-4EJAK4E\mingy <mingyeonging1203@gmail.com>
2022-05-04 15:17:40 +0900

    :art: 완성 코드

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import requests
 
 app = Flask(__name__)
 
-client = MongoClient('아이피', 27017, username="test", password="test")
+client = MongoClient('mongodb://test:test@아이피', 27017)
 db = client.dbsparta_plus_week2
 
 
@@ -22,7 +22,7 @@ def detail(keyword):
     # API에서 단어 뜻 찾아서 결과 보내기
     status_receive = request.args.get("status_give", "old")
     r = requests.get(f"https://owlbot.info/api/v4/dictionary/{keyword}",
-                     headers={"Authorization": "Token 토큰"})
+                     headers={"Authorization": "Token 내 토큰"})
     if r.status_code != 200:
         return redirect(url_for("main", msg="Word not found in dictionary; Try another word"))
     result = r.json()
